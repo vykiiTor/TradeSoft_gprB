a94d8b28155e71ad11676eed41918db21c3d2813
Antonin Boyon <antonin.boyon@gmail.com>
2023-11-23 12:12:36 +0100

    another one

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WritqsdfghjkleLine("Hello this a test from Antonin");
+Console.WritqsdfghjgkleLine("Hello this a test from Antonin");
+fgdfgd
