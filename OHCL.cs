8cd64b1b7d775e79c4fe521c1c776e784199a4ad
vykiiTor <vykiitor@gmail.com>
2024-01-17 21:14:10 +0100

    change regional prblem

--- OHCL.cs
+++ OHCL.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 
 // author: Antonin Boyon
@@ -60,7 +61,7 @@ public class Ticks_Data
                 Ticks_Data data = new Ticks_Data(
                     DateTime.ParseExact(columns[0], "yyyy-MM-dd HH:mm:ss.fff", null),
                     Int32.Parse(columns[2]),
-                    decimal.Parse(columns[3]));
+                    decimal.Parse(columns[3], CultureInfo.InvariantCulture));
                 ticks_Datas.Add(data);
             }
         }
